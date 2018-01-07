fa5904179df8c7e327eee6f1ec06d970d6891d31
Matt <matthewcranford314@gmail.com>
2018-01-06 16:54:09 -0800

    Implement API

--- scripts.js
+++ scripts.js
@@ -1,7 +1,26 @@
 $(document).ready(function(){
 
-  $("#search").click(function() {
-    $("#search-icon-description").text("I don't work yet");
+
+  $("#search").submit(function(event) {
+    var searchInput = $("#search-input").val();
+    var api = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchInput + "&origin=*";
+    // console.log(searchInput);
+    // console.log(api);
+    $.ajax({
+      url: api,
+      type: "GET",
+      success: function(jsonData) {
+        for(i=0; i<jsonData[1].length; i++) {
+          $("#article-list li").each(function(i){
+            $(this).children(".article-title").html(jsonData[1][i]);
+            $(this).children(".article-description").html(jsonData[2][i]);
+            $(this).parent(".article-link").attr("href", jsonData[3][i]);
+            console.log(jsonData[3][i]);
+          });
+        }
+      }  
+    });
+    event.preventDefault();
   });
 
 });
